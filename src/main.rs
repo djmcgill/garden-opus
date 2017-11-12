5418bfe188bbed1fc8b3b8f68cf21a4a5ad61d80
David McGillicuddy <contact@djmcgill.co.uk>
2017-11-12 17:16:26 +0000

    Text rendering prototype

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,11 @@ use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
+use opengl_graphics::{ GlGraphics, OpenGL, GlyphCache, Texture, TextureSettings };
+use graphics::*;
+use graphics::character::CharacterCache;
+use std::fmt::Debug;
+
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
@@ -20,59 +24,55 @@ pub struct App {
 }
 
 impl App {
-        fn render(&mut self, args: &RenderArgs) {
-            use graphics::*;
+    fn render<C, E>(&mut self, args: &RenderArgs, glyphs: &mut C) 
+    where E: Debug, C: CharacterCache<Texture=Texture, Error=E> {
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-            const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-            const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-            let poly = my_graphics::draw_board::unit_hex();
-
-            self.gl.draw(args.viewport(), |c, gl| {
-                // Clear the screen.
-                clear(GREEN, gl);
-                let transform = c.transform.trans(400.0, 400.0).scale(100.0, 100.0);
-                polygon(RED, poly, transform, gl);
-            });
-        }
-
-        fn update(&mut self, args: &UpdateArgs) {
-            let _dt = args.dt;
-        }
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(GREEN, gl);
+            my_graphics::draw_board::draw_hex(c.reset(), gl);
+            let transform = c.transform.trans(400.0, 400.0);
+            text(BLACK, 50, "a", glyphs, transform, gl).unwrap();
+        });
+    }
 }
 
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
+    let opengl_version = OpenGL::V3_2;
 
     let width = 800;
     let height = 800;
     // Create an Glutin window.
     let mut window: Window = WindowSettings::new(
-            "spinning-square",
+            "Garden Opus",
             [width, height]
         )
-        .opengl(opengl)
+        .opengl(opengl_version)
         .exit_on_esc(true)
         .build()
         .unwrap();
+
+    let ref mut glyphs = GlyphCache::new(
+        "assets/FiraSans-Regular.ttf", 
+        (), 
+        TextureSettings::new()
+    ).expect("Could not load font");
+
 
     let center = Vector2::new(width as f64/2.0, height as f64/2.0);
 
     // Create a new game and run it.
     let mut app = App {
-        gl: GlGraphics::new(opengl),
+        gl: GlGraphics::new(opengl_version),
         current_mouse_pos: center,
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new().lazy(true));
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
-            app.render(&r);
-        }
-
-        if let Some(u) = e.update_args() {
-            app.update(&u);
+            app.render(&r, glyphs);
         }
 
         if let Some(xy) = e.mouse_cursor_args() {
--- src/my_graphics/draw_board.rs
+++ src/my_graphics/draw_board.rs
@@ -1,5 +1,6 @@
 use ::model::GameState;
-use graphics::types::Polygon;
+use ::model::elements::Atom;
+use graphics::*;
 
 pub fn draw_board(state: &GameState) {
     
@@ -7,25 +8,43 @@ pub fn draw_board(state: &GameState) {
 
 const HEX_HEIGHT: f64 = 1.0;
 const HEX_WIDTH: f64 = 0.86602540378 * HEX_HEIGHT; // sqrt(3)/2
-pub fn unit_hex() -> Polygon<'static> {
-    &[
-         [HEX_HEIGHT/2.0, 0.0],
-         [HEX_HEIGHT/4.0, HEX_WIDTH/2.0],
-                    [0.0, HEX_WIDTH/2.0],
-        [-HEX_HEIGHT/4.0, HEX_WIDTH/2.0],
-        [-HEX_HEIGHT/2.0, 0.0],
-        [-HEX_HEIGHT/4.0, -HEX_WIDTH/2.0],
-                    [0.0, -HEX_WIDTH/2.0],
-         [HEX_HEIGHT/4.0, -HEX_WIDTH/2.0],
-    ]
+const HEX_COORDS: &'static [[f64; 2]] = &[
+    [ HEX_HEIGHT/2.0,            0.0],
+    [ HEX_HEIGHT/4.0,  HEX_WIDTH/2.0],
+    [            0.0,  HEX_WIDTH/2.0],
+    [-HEX_HEIGHT/4.0,  HEX_WIDTH/2.0],
+    [-HEX_HEIGHT/2.0,            0.0],
+    [-HEX_HEIGHT/4.0, -HEX_WIDTH/2.0],
+    [            0.0, -HEX_WIDTH/2.0],
+    [ HEX_HEIGHT/4.0, -HEX_WIDTH/2.0],
+];
+
+const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+pub fn draw_hex<G>(c: Context, g: &mut G) where G: Graphics {
+    polygon(RED, HEX_COORDS, c.transform, g);
 }
 
-// fn draw_atom(atom: &Atom) {
-//     match *atom {
-//         Atom::BaseElement(element) => {},
-//         Atom::BaseMetal(metal) => {},
-//         Atom::Januae(januae) => {},
-//         Atom::Quicksilver => {},
-//         Atom::Salt => {},
-//     }
-// }
+const FIRE_SYMBOL: & 'static str = "🜂";
+const AIR_SYMBOL: & 'static str = "🜁";
+const WATER_SYMBOL: & 'static str = "🜄";
+const EARTH_SYMBOL: & 'static str = "🜃";
+const LEAD_SYMBOL: & 'static str = "♄";
+const TIN_SYMBOL: & 'static str = "♃";
+const IRON_SYMBOL: & 'static str = "♂";
+const COPPER_SYMBOL: & 'static str = "♀";
+const SILVER_SYMBOL: & 'static str = "☽";
+const GOLD_SYMBOL: & 'static str = "☉";
+const VITAE_SYMBOL: & 'static str = "🜍";
+const MORS_SYMBOL: & 'static str = "🜞";
+const QUICKSILVER_SYMBOL: & 'static str = "☿";
+const SALT_SYMBOL: & 'static str = "🜔";
+
+fn draw_atom(atom: &Atom) {
+    match *atom {
+        Atom::BaseElement(element) => {},
+        Atom::BaseMetal(metal) => {},
+        Atom::Januae(januae) => {},
+        Atom::Quicksilver => {},
+        Atom::Salt => {},
+    }
+}
