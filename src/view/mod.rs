6e4ed3367c209f0ebf22a226b2004bbcc187706a
David McGillicuddy <contact@djmcgill.co.uk>
2017-11-15 12:04:16 +0000

    Minor improvement to hex coords

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -12,20 +12,24 @@ impl BoardView {
         BoardView
     }
 
+    fn hex_ix_offset(ix: usize) -> (f64, f64) {
+        let (x, y) = IX_TO_COORDS[ix];
+        let yf = y as f64;
+        let xf = x as f64;
+        let x_x = 1.0 * draw_board::HEX_WIDTH;
+        let x_y = 0.0 * draw_board::HEX_HEIGHT;
+        let y_x = -0.5 * draw_board::HEX_WIDTH;
+        let y_y = 0.75 * draw_board::HEX_HEIGHT;
+        (xf * x_x + yf * y_x, xf * x_y + yf * y_y) // TODO: replace with linalg operations
+    }
+
     pub fn draw<G: Graphics, E: Debug, C>(&self, controller: &BoardController, c: &Context, g: &mut G, glyphs: &mut C)
     where C: CharacterCache<Texture=G::Texture, Error=E> {
         let base_c = c.reset().scale(0.1, 0.1);
         for i in 0..CELL_COUNT {
-            let (y, x) = IX_TO_COORDS[i];
-            let yf = y as f64;
-            let xf = x as f64;
-            let x_x = 1.0 * draw_board::HEX_WIDTH;
-            let x_y = 0.0;
-            let y_x = -0.5 * draw_board::HEX_WIDTH;
-            let y_y = 0.75 * draw_board::HEX_HEIGHT;
-            let new_c = base_c
-                .trans(xf * x_x, xf * x_y)
-                .trans(yf * y_x, yf * y_y);
+            
+            let (x_offset, y_offset) = Self::hex_ix_offset(i);
+            let new_c = base_c.trans(x_offset, y_offset);
             draw_board::draw_hex(new_c, g);            
         }
 
